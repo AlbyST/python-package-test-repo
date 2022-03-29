4fa206879e8dc86e4c99a85076c56d161fed2fd1
Alberto Villa <alberto.villa1@st.com>
2022-03-29 09:31:03 +0200

    Add setUp() and tearDown() methods to test class

--- tests/test.py
+++ tests/test.py
@@ -3,5 +3,11 @@ import unittest
 import python_test_package
 
 class MyTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
     def test(self):
         self.assertEqual(python_test_package.extremely_useful_function(), 42)
